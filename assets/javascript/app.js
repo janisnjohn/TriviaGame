a260c590cd3db83a523096c1350c9d0b36f9d36a
janisnjohn <janisnjohn@gmail.com>
2017-08-17 16:21:24 -0700

    with correct answer fix

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 // start button click.  display: none
 
 // number of seconds to complete the answer.
-var number = 30;
+var number = 20;
 var intervalId;
 
 // timer starts 15 seconds to answer question once start button is pushed.
@@ -12,37 +12,41 @@ var intervalId;
 //list of questions, choices and answer. 
 var pos = 0, test, test_status, question, choice, choices, chA, chB, chC, chD, wrong=0, correct = 0, unanswered=0;
 var questions=[
-	["The Eiffel Tower is located where in Paris?", "Bois de Boulogne", "Champ de Mars", "Jardin des Plantes", "Parc de Belleville", "B", "assets/images/EiffelTower.gif"],
-	["Where is the London Bridge located?", "Paris", "Champ de Mars", "Lake Havasu City", "London", "C", "assets/images/Bridge.gif"],
-	["Which is the most populated city in the world?", "New York City", "Tokyo Japan", "Delhi India", "Shanghai China", "B", "assets/images/Tokyo.gif"],
-	["Where is Mount Rushmore located?", "Washington DC", "Minot North Dakota", "Keystone South Dakota", "Cheyenne Wyoming", "C", "assets/images/MountRushmore.gif"],
-	["What city is the Taj Mahal in?", "Dehli", "Agra", "Kanpur", "Patna", "B", "assets/images/TajMahal.gif"]
+	["The Eiffel Tower is located where in Paris?", "Bois de Boulogne", "Champ de Mars", "Jardin des Plantes", "Parc de Belleville", "B", "assets/images/EiffelTower.gif", "Champ de Mars"],
+	["Where is the London Bridge located?", "Paris", "Rome", "Lake Havasu City", "London", "C", "assets/images/Bridge.gif", "Lake Havasu City"],
+	["Which is the most populated city in the world?", "New York City", "Tokyo Japan", "Delhi India", "Shanghai China", "B", "assets/images/Tokyo.gif", "Tokyo Japan"],
+	["Where is Mount Rushmore located?", "Washington DC", "Minot North Dakota", "Keystone South Dakota", "Cheyenne Wyoming", "C", "assets/images/MountRushmore.gif", "Keystone South Dakota"],
+	["What city is the Taj Mahal in?", "Dehli", "Agra", "Kanpur", "Patna", "B", "assets/images/TajMahal.gif", "Agra"]
 ];
 
-function _(x){
-	return document.getElementById(x);
-}
+// function _(x){
+// 	return document.getElementById(x);
+// }
 
 function renderQuestion() {
-	test = _("test");
+	test = $("#test");
+	// _("test_status").innerHTML = "Question " +(pos+1) + " of " +questions.length;
 	if(pos >= questions.length){
-		$("#test").html("<h2> Correct: " + correct+ "<br>Wrong: " + wrong +"</h2>");
+		clearInterval(intervalId);
+		$("#test").html("<center><h2> Correct: " + correct+ "<br>Wrong: " + wrong + "<br>Unanswered: " +unanswered+ "</h2></center>");
 		$("#test").append("<center><img src='assets/images/GameOver.gif'></center>");
+		console.log("the end");	
+		setTimeout(restart, 4000);
 		pos =0;
 		correct = 0;
 		wrong = 0;
-		clearInterval(intervalId);
-		return false;
-		// $("#start").html("<button>restart</button>");
-		// $("#start").on("click", renderQuestion);
+		unanswered =0;
+	
 
 	}
-	// _("test_status").innerHTML = "Question " +(pos+1) + " of " +questions.length;
+
+	else{
 	question = questions[pos][0];
 	chA = questions[pos][1];
 	chB = questions[pos][2];
 	chC = questions[pos][3];
 	chD = questions[pos][4];
+
 	$("#test").html("<h3>" + question + "</h3>");
 	$("#test").append("<input type='radio' name='choices' value='A'> "+chA + "<br>");
 	$("#test").append("<input type='radio' name='choices' value='B'> "+chB + "<br>");
@@ -52,6 +56,12 @@ function renderQuestion() {
 	$("#submit").on("click", checkAnswer);
 	console.log(checkAnswer);
 
+}
+
+}	
+function restart(){
+		$("#start").append("<center><button>restart</button></center>");
+		$("#start").on("click", run);
 }
 function checkAnswer(){
 	choices = document.getElementsByName("choices");
@@ -63,18 +73,21 @@ function checkAnswer(){
 	}
 	if (choice === questions[pos][5]) {
 		correct++;
-		console.log("corect11111");
 		$("#test").html("<center><h3>You Guessed Correct!</h3></center>");
+		$("#test").append("<center><img src='" +questions[pos][6]+"'></center>");
+	} else if (choice == undefined) {
+		unanswered++;
+		$("#test").html("<center><h3>Sorry the correct answer is "+questions[pos][7] +"</center>");
 		$("#test").append("<center><img src='" +questions[pos][6]+"'></center>");
 	} else {
 		wrong++;
-		$("#test").html("<center><h3>Sorry the correct answer is "+questions[pos][5] +"</center>");
+		$("#test").html("<center><h3>Sorry the correct answer is "+questions[pos][7] +"</center>");
 		$("#test").append("<center><img src='" +questions[pos][6]+"'></center>");
 	}
 	pos++;
-	number=33;
+	number=22;
 
-	setTimeout(renderQuestion, 3000);
+	setTimeout(renderQuestion, 2000);
 }
 
 function run() {
@@ -86,22 +99,22 @@ function decrement() {
 
 	number--;
 
-	$("#timer").html("<h2>Time Remaining: " + number + " seconds</h2>");
+	$("#start").html("<h2>Time Remaining: " + number + " seconds</h2>");
 
 	if (number === 0) {
-
-	alert("Times Up!");
 	pos++;
-	number=33;
+	number=24;
 	unanswered++;
-	setTimeout(renderQuestion, 3000);
+	checkAnswer();
+	setTimeout(renderQuestion, 4000);
+
 	}
 }
 
-	$("#start").html("<button>start</button>");
+	$("#start").html("<center><button>start</button></center>");
 	$("#start").on("click", run);
 
-window.addEventListener("load", renderQuestion, false);
+// window.addEventListener("load", renderQuestion, false);
 });
 
 
